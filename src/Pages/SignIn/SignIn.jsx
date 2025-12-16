e3501175142889dc2e2ba9c214fa7c0ba31b7ae9
sahaantara567 <sahaantara567@gmail.com>
2025-12-16 14:13:25 +0600

    added user section

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -4,28 +4,36 @@ import { Link } from 'react-router';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../Firebase/Firebase.config';
 import { toast } from 'react-toastify';
+import { IoEyeOff } from "react-icons/io5";
+import { FaEye } from "react-icons/fa";
+import { useState } from 'react';
 
 const SignIn = () => {
+     
+    const [user, setUser] = useState(null);
+    const [show, setShow] = useState(false);
      const handleSignIn =(e) =>{
         e.preventDefault();
         const email = e.target.email?.value;
         const password = e.target.password?.value;
         console.log('SignIn clicked',{email,password});
          
-        const handleGoogleSignIn = () => {
-        console.log('Google Sign-In clicked');
-    };
+    //     const handleGoogleSignIn = () => {
+    //     console.log('Google Sign-In clicked');
+    // };
 
-        signInWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth,email,password)
             .then((res) =>{
             console.log(res);
-            toast.success('User logged in successfully');
+            setUser(res.user);
+            toast.success('User Sign in successfully');
         })
         .catch((error) =>{
             console.log(error);
             toast.error(error.message);
         });
-    }
+    };
+    console.log(user);
     return (
         <div className="flex justify-center items-center bg-base-200 mb-40">
   <div className="hero-content  w-120 flex-col lg:flex-row-reverse">
@@ -33,21 +41,49 @@ const SignIn = () => {
      
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <h2 className='font-semibold text-2xl text-cyan-700 ml-15 mt-2'>Login Your Account</h2>
+        <h2 className='font-semibold text-2xl text-cyan-700 ml-15 mt-2'>SignIn Your Account</h2>
       <div className="card-body">
-        <form  onSubmit={''}  className="form">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover ml-25 mt-5">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4 items-center ml-25 w-30">Signin</button>
+       { user ? (<div className='text-center space-y-3'>
+        <img src={user?.photoURL || "https://tse3.mm.bing.net/th/id/OIP.vWf_ciadzIXclB2o4YOFFQHaNL?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3"} className='w-20 h-20 rounded-full mx-auto' alt=""/>
+        <h2  className='text-xl font-semibold'>{user?.displayName}</h2>
+       </div>) : ( <form  onSubmit={handleSignIn}  className="form">
+           <div>
+                <label className="block text-sm font-medium mb-1">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="example@email.com"
+                  className="input input-bordered w-full bg-white/20 text-black placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                />
+            
+          </div>
+         
+             <div className="relative">
+                          <label className="block text-sm font-medium mb-1">
+                            Password
+                          </label>
+                          <input
+                            type={show ? "text" : "password"}
+                            name="password"
+                            placeholder="••••••••"
+                            className="input input-bordered w-full bg-white/20 text-black placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                          />
+                          <span
+                            onClick={() => setShow(!show)}
+                            className="absolute right-[8px] top-[36px] cursor-pointer z-50"
+                          >
+                            {show ? <FaEye /> : <IoEyeOff />}
+                          </span>
+                        </div>
+          
+          <button className="btn btn-neutral mt-5 items-center ml-25 w-30">Sign In</button>
+          <div><a className="link link-hover ml-25 mb-6">Forgot password?</a></div>
           <div className='flex item-center justify-center my-2 gap-2'>
             <div className='h-px w-16 bg-amber-100 mt-2'></div>
             <span className='mx-2 text-gray-400 font-bold'>OR</span>
             <div className='h-px w-16 bg-amber-100 mt-2'></div>
           </div>
-          <button type='button' onClick={handleGoogleSignIn} className="btn btn-outline btn-warning mt-4 ml-16 ">
+          <button type='button' onClick={''} className="btn btn-outline btn-warning mt-4 ml-16 ">
              <img
                   src="https://www.svgrepo.com/show/475656/google-color.svg"
                   alt="google"
@@ -57,7 +93,7 @@ const SignIn = () => {
 
           </button>
           <p className='font-bold text-gray-400 text-1xl ml-14 pt-3 '>Don't have an account ? {''}<Link to={'/auth/signup'} className='text-red-900'>SignUp </Link></p>
-        </form>
+        </form>)}
       </div>
     </div>
   </div>
--- src/Pages/SignUp/SignUP.jsx
+++ src/Pages/SignUp/SignUP.jsx
@@ -3,17 +3,30 @@ import { Link } from 'react-router';
 import {  createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../Firebase/Firebase.config';
 import { toast } from 'react-toastify';
+import { IoEyeOff } from "react-icons/io5";
+import { FaEye } from "react-icons/fa";
+import { useState } from 'react';
 
 const SignUP = () => {
-       const SignUp = () =>{
-        const handleSignUp = (e) =>{
+
+  const [show, setShow] = useState(false);
+       
+        const handleSignUp =  (e) =>{
             e.preventDefault();
             const email = e.target.email?.value;
             const password = e.target.password?.value;
+
         
-            console .log("signUp function entered" , {email,password});
-        };
-           createUserWithEmailAndPassword(auth, email, password)  
+            console.log("signUp function entered", { email, password });
+            
+            
+            if (!password ||password.length < 6){
+                toast.error('Password must be at least 6 characters long');
+            console.log(password.length);
+                return;
+            }
+      
+             createUserWithEmailAndPassword(auth, email, password)  
                     .then((res) =>{
                     console.log(res);
                     toast.success('User SignUp successfully');
@@ -34,24 +47,46 @@ const SignUP = () => {
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <h2 className='font-semibold text-2xl text-cyan-700 ml-15 mt-2'>SignUp Your Account</h2>
-      <div className="card-body">
+      <form onSubmit={handleSignUp}  className="card-body">
         
           <label className="label">Name</label>
-          <input type="text" className="input" placeholder="Name" />
+          <input type="text"  className="input" placeholder="Name" />
 
           <label className="label">Photo URL</label>
-          <input type="text" className="input" placeholder="Photo URL" />
+          <input type="text" name="photo URL" className="input" placeholder="Photo URL" />
 
          
+            <div>
+                <label className="block text-sm font-medium mb-1">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="example@email.com"
+                  className="input input-bordered w-full bg-white/20 text-black placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                />
+            
+          </div>
+          <div className="relative">
+                <label className="block text-sm font-medium mb-1">
+                  Password
+                </label>
+                <input
+                  type={show ? "text" : "password"}
+                  name="password"
+                  placeholder="••••••••"
+                  className="input input-bordered w-full bg-white/20 text-black placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                />
+                <span
+                  onClick={() => setShow(!show)}
+                  className="absolute right-[8px] top-[36px] cursor-pointer z-50"
+                >
+                  {show ? <FaEye /> : <IoEyeOff />}
+                </span>
+              </div>
           
-          <label className="label">email</label>
-          <input type="email" className="input" placeholder="email" />
-          <label className="label">password</label>
-          <input type="password" className="input" placeholder="password" />
-          
-          <button className="btn btn-neutral mt-4">SignUp</button>
+          <button type= "submit" className="btn btn-neutral mt-4">SignUp</button>
           <p className='font-bold text-gray-400 text-1xl ml-14 pt-3 '>Already have an account ? {''}<Link to={'/auth/signin'} className='text-red-900'>SignIn </Link></p>
-        </div>
+        </form>
       </div>
     </div>
   </div>
